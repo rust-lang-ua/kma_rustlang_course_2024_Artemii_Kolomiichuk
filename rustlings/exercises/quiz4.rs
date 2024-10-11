190e334b30a25ca753abeeb357419773a462d543
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2024-10-11 16:46:03 +0300

    finish macros and quiz4

--- rustlings/exercises/quiz4.rs
+++ rustlings/exercises/quiz4.rs
@@ -5,7 +5,20 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+// I AM DONE
+
+macro_rules! my_macro {
+    () => {
+        println!("Check out my macro!");
+    };
+    ($val:expr) => {
+        {
+            let mut s = "Hello ".to_string();
+            s.push_str($val);
+            s
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
